389443f7f808393b31acf94ac2f0cd4c3eca0071
nyanmyohtet <nyanmyohtet.bx@gmail.com>
2024-11-09 18:22:31 +0700

    update database.config

--- config/database.config.php
+++ config/database.config.php
@@ -2,7 +2,7 @@
 return [
     'host' => 'localhost',
     'port' => '3306',
-    'db_name' => 'php_web_app',
-    'username' => 'username',
-    'password' => 'password',
+    'db_name' => 'coffee-shop-finder',
+    'username' => 'root',
+    'password' => 'root',
 ];
